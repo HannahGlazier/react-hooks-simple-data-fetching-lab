8eb2a536a9c7a17469d782fbb7db6d789a3ebeb1
Hannah Glazier <hrayglazier@aol.com>
2022-03-01 07:41:15 -0700

    commit tree d783b97672914d6f24d1bd58c0d02a8ce5826091

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useState, useEffect } from 'react'
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+
+function App() {
+
+    const [dogPic, setDogPic] = useState(null)
+
+    useEffect(() => {
+        fetch(url)
+        .then(response => response.json()) 
+        .then((data) => {
+            setDogPic(data.message)
+        })
+    }, [])
+
+    if(!dogPic) return <p>Loading...</p>
+
+  return (
+    <img src={dogPic} alt="A Random Dog"></img>
+  )
+}
+
+export default App
